5651f38db3d19515f4df10443124286863655f4a
KAMLESH RATANPARA <rkjrk514@gmail.com>
2023-02-25 12:36:42 +0530

    chengis

--- nova.py
+++ nova.py
@@ -1,25 +1,28 @@
-import nova_engine as ne    
+import nova_engine as ne
+from waiting import waiting_mode   
 start_nova= ne.speech_to_text().lower()
-if "nova" in start_nova or 1:
-    ne.text_to_speech("  All  NOVA  System  is  online  ".upper()+"Hey Master , Wellcome To NOVA , How Can I Help You ?".upper())
+if "nova".lower() in start_nova or 1:
+    from wish import wish_user
+    wish_user()
+    # ne.text_to_speech("  All  NOVA  System  is  online  ".upper()+"Wellcome To NOVA , How Can I Help You ?".upper())
     while True:
         speech_data=ne.speech_to_text().lower()
         #..................normal command's...................
         
-        if "time now" in speech_data:
+        if "time now".lower() in speech_data:
             temp=ne.datetime.datetime.now().strftime("%I Hour, %M Minite , %S Secound , %p")
                 # print(time)
             ne.text_to_speech("current time is "+temp)
         
-        elif "how are you" in speech_data:
+        elif "how are you".lower() in speech_data:
             ne.text_to_speech(" I AM FINE THANK YOU KAMLESH , YOU ARE SO CARING PERSON ")
         
-        elif "go and sleep" in speech_data or "exit" in speech_data:
+        elif "go and sleep".lower() in speech_data or "exit".lower() in speech_data:
             ne.text_to_speech("all system goes to offline master".upper())
             ne.text_to_speech("i hope you  come to me in near future ".upper())
             break
         
-        elif "tell about you" in speech_data:
+        elif "tell about you".lower() in speech_data:
             ne.text_to_speech('''
                     my name is nova,
                     i was created by kamlesh ratanpara,
@@ -32,31 +35,31 @@ if "nova" in start_nova or 1:
             print("my creater gmail id is ".upper()+" rkjrk514@gmail.com")
             print("my creater phone number is".upper()+" 8866151630")
 
-        elif "which date today" in speech_data:
+        elif "which date today".lower() in speech_data:
             date=ne.datetime.datetime().date()
             ne.text_to_speech(date.today())
         
-        elif " tell me joke" in speech_data:
-           joke= ne.pyjokes.get_joke(language= "guj")
-           print(joke)
+        elif " tell me joke".lower() in speech_data:
+            joke= ne.pyjokes.get_joke(language= "guj")
+            print(joke)
         #.............. SYSTEM COMMAND'S OR OS COMMAND'S ................
-        elif "open notepad" in speech_data:
+        elif "open notepad".lower() in speech_data:
             notepade_path="C://Windows//System32//notepad.exe"
             ne.os.startfile(notepade_path)
         
-        elif "open sublime text editor" in speech_data:
+        elif "open sublime text editor".lower() in speech_data:
             sublime_path="C://Program Files//Sublime Text//sublime_text.exe"
             ne.os.startfile(sublime_path)
-        elif "open android studio" in speech_data:
+        elif "open android studio".lower() in speech_data:
             android_path="C://Program Files//Android//Android Studio//bin//studio64.exe"
             ne.os.startfile(android_path)
-        elif "play music" in speech_data:
+        elif "play music".lower() in speech_data:
             song_path="D://2//song"
             songs=ne.os.listdir(song_path)
             random_song_choies=ne.random.choice(songs)
             ne.os.startfile(ne.os.path.join(song_path,random_song_choies))
         #..................NETWORK COMMAND'S...........................
-        elif "open youtube" in speech_data:
+        elif "open youtube".lower() in speech_data:
             ne.text_to_speech("which channel you want to see ".upper())
             speech_data_temp=ne.speech_to_text().lower()
             temp=speech_data_temp.split(" ")
@@ -70,19 +73,19 @@ if "nova" in start_nova or 1:
             except Exception as e:
                 pass
         
-        elif " open website" in speech_data:
+        elif " open website".lower() in speech_data:
             ne.text_to_speech("your webbrowser is started ".upper())
             ne.text_to_speech("tell me which website you want to open".upper())
             temp=ne.speech_to_text().lower()
             # temp=speech_dat
             ne.webbrowser.open_new_tab("https://www.google.com/search?q="+temp)
         
-        elif "ip address" in speech_data:
+        elif "ip address".lower() in speech_data:
             ip=ne.requests.get('https://api.ipify.org').text
             print(f"your ip address is {ip}".upper())
             ne.text_to_speech(f"your ip address is ,{ip}")
 
-        elif "send email to kamlesh" in speech_data:
+        elif "send email to kamlesh".lower() in speech_data:
             try:
                 print("what should i send in email")
                 ne.text_to_speech("what should i send in email")
@@ -94,22 +97,24 @@ if "nova" in start_nova or 1:
             except Exception as e:
                 print(e)
                 ne.text_to_speech("sorry,i can not send Email ")
-        elif "send whatsapp " in speech_data or "send massage " in speech_data:
+        elif "send whatsapp".lower() in speech_data or "send massage " in speech_data:
             to= ne.getReciverNumber()
             ne.text_to_speech("what should i send ")
             content=ne.speech_to_text().lower()
-            ne.pywhatkit.sendwhatmsg_instantly(to,content)
+            ne.pywhatkit.sendwhatmsg_instantly(phone_no=to,message=content)
             ne.text_to_speech(f"message has been send to {to}")
-        elif "play song on youtube" in speech_data:
+        elif "play youtube song".lower() in speech_data:
             ne.text_to_speech("which song i play on youtube")
             ytsong_name=ne.speech_to_text().lower()
             ne.pywhatkit.playonyt(ytsong_name)
-        elif "wikipedia" in speech_data:
+            ne.time.sleep(5)
+            waiting_mode()
+        elif "wikipedia".lower() in speech_data:
             ne.text_to_speech("what should i search on wikipedia")
             while True:
                 wiki_search=ne.speech_to_text().lower()
                 try:
-                    result=ne.wikipedia.summary(wiki_search)
+                    result=ne.wikipedia.summary(wiki_search,3)
                     print(result)
                     ne.text_to_speech(result)
                     break
@@ -118,7 +123,13 @@ if "nova" in start_nova or 1:
                     i found many many information about {wiki_search},
                     please tell me exactly what you want to search on wikipedia
                     ''')
-
+        elif "wait".lower() in speech_data:
+            from win32gui import GetWindowText, GetForegroundWindow
+            currentwindow=str(GetWindowText(GetForegroundWindow()))
+            while "nova.py" not in currentwindow:
+                ne.time.sleep(2)
+                currentwindow=str(GetWindowText(GetForegroundWindow()))
+            ne.text_to_speech("again ,welcome sir,what can i do for you?")
         ne.time.sleep(0.5)
 else:
     ne.text_to_speech("ohh shit,Nova System failear ".upper())
